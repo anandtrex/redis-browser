0de06b3ec8586d6b1009fedd2f3173fdca0e4bce
Anand <anandtrex@users.noreply.github.com>
2018-10-28 20:50:23 +0100

    Adding menu for macos

--- src/menu/mainmenu.js
+++ src/menu/mainmenu.js
@@ -19,8 +19,23 @@ if (process.platform === 'darwin') {
       {role: 'quit'}
     ]
   })
+
+  template.push({
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' }
+        ]
+      })
 }
 
 const menu = Menu.buildFromTemplate(template)
-// Menu.setApplicationMenu(menu)
-Menu.setApplicationMenu(null)
+Menu.setApplicationMenu(menu)
+// Menu.setApplicationMenu(null)
